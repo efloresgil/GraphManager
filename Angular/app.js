2ee8bf4dcbb99aea91c8c875ff598a64e32d31bc
E. Flores <edgarfloresgil@gmail.com>
2016-03-25 16:49:14 -0600

    Agregado el atributo grado para los vértices

--- Angular/app.js
+++ Angular/app.js
@@ -1,8 +1,28 @@
 var app = angular.module("GraphManager", []);
 
 app.controller('mainCtrl', function($scope) {
-  $scope.bidireccional=true;
+  var first = {
+    "id": 1,
+    "color": "#eee",
+    "grado": 0
+  };
+  $scope.matriz = [];
+  $scope.vertices = [first];
+
+  $scope.bidireccional = true;
   //variables
+  function calcularGrado(vertice) {
+    var grado;
+    var item;
+    for (var i = 0; i < $scope.matriz.length; i++) {
+      item = $scope.matriz[i];
+      if (item.salida===vertice.id) {
+        grado++;
+      }
+    }
+    return {"grado":grado, "par":((grado%2)===0)}
+  }
+
   function getRandomColor() {
     var length = 6;
     var chars = '0123456789ABCDEF';
@@ -10,7 +30,6 @@ app.controller('mainCtrl', function($scope) {
     while (length--) hex += chars[(Math.random() * 16) | 0];
     return hex;
   }
-
 
   function nextID() {
     var mayor = 1;
@@ -26,18 +45,14 @@ app.controller('mainCtrl', function($scope) {
   Vertice = function(id, color) {
     this.id = nextID();
     this.color = getRandomColor();
+    this.grado=0;
   }
   Celda = function(salida, llegada, costo) {
     this.salida = salida;
     this.llegada = llegada;
-    this.costo=costo;
+    this.costo = costo;
   }
-  var first = {
-    "id": 1,
-    "color": "#eee"
-  };
-  $scope.matriz = [];
-  $scope.vertices = [first];
+
 
   $scope.nuevoVertice = function() {
     var vertice = new Vertice(1, "#eee");
@@ -47,8 +62,8 @@ app.controller('mainCtrl', function($scope) {
   }
 
   $scope.nuevaConexion = function() {
-    var msg="";
-    var msgBi=""
+    var msg = "";
+    var msgBi = ""
     var repetido;
     var repetidoBi;
     var conexion;
@@ -64,30 +79,32 @@ app.controller('mainCtrl', function($scope) {
       alert("Selecciona los vértices de salida y llegada");
     } else {
       for (var i = 0; i < $scope.matriz.length; i++) {
-        if ($scope.matriz[i].salida===salida && $scope.matriz[i].llegada===llegada) {
-          repetido=true;
+        if ($scope.matriz[i].salida === salida && $scope.matriz[i].llegada === llegada) {
+          repetido = true;
           msg += salida + " -> " + llegada + " ya existe, por lo que no fue creado \n";
           break;
         }
       }
       if (!repetido) {
-        conexion=new Celda(salida,llegada,costo);
+        conexion = new Celda(salida, llegada, costo);
         $scope.matriz.push(conexion);
         msg += salida + " -> " + llegada + " agregado \n";
+        $scope.selectedSalida.grado++;
       }
 
       if ($scope.bidireccional && !circular) {
         for (var i = 0; i < $scope.matriz.length; i++) {
-          if ($scope.matriz[i].salida===llegada && $scope.matriz[i].llegada===salida) {
-            repetidoBi=true;
+          if ($scope.matriz[i].salida === llegada && $scope.matriz[i].llegada === salida) {
+            repetidoBi = true;
             msgBi += llegada + " -> " + salida + " ya existe, por lo que no fue creado";
             break;
           }
         }
         if (!repetidoBi) {
-          conexion=new Celda(llegada,salida,costo);
+          conexion = new Celda(llegada, salida, costo);
           $scope.matriz.push(conexion);
           msgBi += llegada + " -> " + salida + " agregado";
+          $scope.selectedLlegada.grado++;
         }
       }
     }
@@ -99,19 +116,19 @@ app.controller('mainCtrl', function($scope) {
 
   $scope.eliminarVertice = function(id) {
     //alert(id);
-    $scope.matriz=$scope.matriz.filter(function(item){
-      return (item.salida!==id && item.llegada!==id);
+    $scope.matriz = $scope.matriz.filter(function(item) {
+      return (item.salida !== id && item.llegada !== id);
     });
 
-    $scope.vertices=$scope.vertices.filter(function(item){
-      return item.id!==id;
+    $scope.vertices = $scope.vertices.filter(function(item) {
+      return item.id !== id;
     });
   }
 
   $scope.eliminarConexion = function(conexion) {
     //alert(id);
-    $scope.matriz=$scope.matriz.filter(function(item){
-      return (item.salida!==conexion.salida || item.llegada!==conexion.llegada);
+    $scope.matriz = $scope.matriz.filter(function(item) {
+      return (item.salida !== conexion.salida || item.llegada !== conexion.llegada);
     });
   }
 
